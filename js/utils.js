23c8c5ca908e62acb46906361304d4b6dc89609b
s-jez <stanislawjez7@gmail.com>
2022-08-29 19:54:42 +0200

    Finished rendering a list.

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,43 @@
+const createInfoItem = (labelName, value) => {
+  const infoEl = document.createElement("div");
+
+  const labelEl = document.createElement("strong");
+  labelEl.innerText = labelName + ": ";
+
+  const valueEl = document.createElement("span");
+  valueEl.innerText = value;
+
+  infoEl.appendChild(labelEl);
+  infoEl.appendChild(valueEl);
+
+  return infoEl;
+};
+
+const createFlagItem = (country) => {
+  const imgDivContainer = document.createElement("div");
+  const img = document.createElement("img");
+  img.src = country.flagsURL;
+  img.width = 160;
+  img.height = 98;
+
+  imgDivContainer.appendChild(img);
+
+  return imgDivContainer;
+};
+
 const createCountryItem = (country) => {
   const li = document.createElement("li");
-  li.innerText = country.name;
+
+  const liName = document.createElement("span");
+  liName.innerText = country.name;
+
+  li.appendChild(createFlagItem(country));
+  li.appendChild(liName);
+
+  li.appendChild(createInfoItem("Population", country.population));
+  li.appendChild(createInfoItem("Region", country.region));
+  li.appendChild(createInfoItem("Capital", country.capital));
+
   return li;
 };
 
